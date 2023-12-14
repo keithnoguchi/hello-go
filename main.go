144de833fd569dfe6d7eefe6a70d092ba6ade483
Keith Noguchi <keith@noguchi.us>
2023-12-13 17:07:34 -0800

    main.go: A simple HTTP hello world server

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 // SPDX-License-Identifier: GPL-2.0
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
 }
